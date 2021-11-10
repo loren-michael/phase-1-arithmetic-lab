180eb6b39a82b6ed9314719948c5778b84444e43
loren <lorenmichaelh@gmail.com>
2021-11-10 14:53:20 -0600

    commit tree e82656582df8affa1a0ce0ac8ef9b42723f28219

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const chai = 'chai';
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(2, 20, 17, 5, 13);
